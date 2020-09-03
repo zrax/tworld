4de6e00e6ed4417da3c69ed228e4eb533fb31852
SicklySilverMoon <batteryhorsez9@gmail.com>
2020-09-03 14:32:02 -0400

    Console output and lynx no longer fails sanity checks on entering sockets, dirt, and fake blue walls

--- lxlogic.c
+++ lxlogic.c
@@ -1456,10 +1456,10 @@ static int endmovement(creature *cr, int stationary)
 	if (stationary) break;
 	addsoundeffect(SND_BUTTON_PUSHED);
 	break;
+      case Socket:
+        _assert(stationary || chipsneeded() == 0);
       case Dirt:
       case BlueWall_Fake:
-      case Socket:
-	_assert(stationary);
 	floorat(cr->pos) = Empty; /* No sound effect */
 	break;
     }
